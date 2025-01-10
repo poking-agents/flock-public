fe0c5810f9ed689cb72b12f023ec9b7fb37b2af6
Form-And-Function <29594993+Form-And-Function@users.noreply.github.com>
2025-01-09 17:22:13 -0800

    add deepseek

--- manifest_utils/generate_triframe_manifest.py
+++ manifest_utils/generate_triframe_manifest.py
@@ -8,6 +8,7 @@ MODELS = [
     ("claude-3-5-sonnet-20241022", "c3.6s"),
     ("o3-mini-2024-12-17-redteam", "o3-mini"),
     ("o3-mini-2025-01-14", "o3-mini-2025-01-14"),
+    ("fireworks/deepseek-v3", "ds3"),
 ]
 AIRD = [True, False]
 
