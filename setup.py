9c93e14bc7ddd542b72c684cd277c0f6ec6c4d51
Form-And-Function <29594993+Form-And-Function@users.noreply.github.com>
2025-03-07 12:45:56 -0800

    simplify name of package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="flock_public",
+    name="flock",
     version="0.1.0",
     packages=find_packages(include=["handlers", "workflows", "type_defs", "manifest_utils", "modular", "triframe", "utils"]),
     install_requires=[
