22fe4827b9270ef3b4da852384823ebe04935549
Form-And-Function <29594993+Form-And-Function@users.noreply.github.com>
2025-03-07 12:42:49 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flock-public",
+    version="0.1.0",
+    packages=find_packages(include=["handlers", "workflows", "type_defs", "manifest_utils", "modular", "triframe", "utils"]),
+    install_requires=[
+        # Add dependencies from requirements.txt here
+    ],
+    python_requires=">=3.7",
+)
