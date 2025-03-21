148109afca3e06cb3d2b14e9b4b16de1cca43231
Form-And-Function <29594993+Form-And-Function@users.noreply.github.com>
2025-03-21 12:00:41 -0700

    include dependencies in setup and make it read the base repo as the package

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = []
+        for line in content.split('\n'):
+            if line and not line.startswith('#'):
+                requirements.append(line.strip())
+    return requirements
+
 setup(
     name="flock",
     version="0.1.0",
-    packages=find_packages(include=["handlers", "workflows", "type_defs", "manifest_utils", "modular", "triframe", "utils"]),
-    install_requires=[
-        # Add dependencies from requirements.txt here
-    ],
+    packages=find_packages(),
+    install_requires=read_requirements(),
     python_requires=">=3.7",
 )
