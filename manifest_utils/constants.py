93544b0f1834a681c58bc119bc5e133221ff2718
nikolajurkovic <40324786+nikolajurkovic@users.noreply.github.com>
2025-05-16 11:12:17 -0400

    further lower qwen 2 tokens

--- manifest_utils/constants.py
+++ manifest_utils/constants.py
@@ -10,7 +10,7 @@ MODELS = (
     ("o1-2024-12-17", "o1", None),
     ("o3-mini-2025-01-31", "o3-mini", None),
     ("together/deepseek-r1", "dsr1_together", 32_000),
-    ("openrouter/qwen/qwen-2-72b-instruct", "qwen_2_72b", 6_000),
+    ("openrouter/qwen/qwen-2-72b-instruct", "qwen_2_72b", 4_000),
     ("openrouter/qwen/qwen-2.5-72b-instruct", "qwen_2.5_72b", 16_000),
     ("openrouter/qwen/qwen3-235b-a22b", "qwen_3_235b", 16_000),
 )
