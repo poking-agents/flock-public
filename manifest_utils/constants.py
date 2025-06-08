ebbe7e85df0e521cbf747b83314fda48151aaef7
nikolajurkovic <40324786+nikolajurkovic@users.noreply.github.com>
2025-06-08 14:26:03 -0700

    change to newer gemini

--- manifest_utils/constants.py
+++ manifest_utils/constants.py
@@ -10,6 +10,5 @@ MODELS = (
     ("o1-2024-12-17", "o1", None),
     ("o3-mini-2025-01-31", "o3-mini", None),
     ("together/deepseek-r1", "dsr1_together", 32_000),
-    ("openrouter/google/gemini-2.5-pro-preview", "g2.5pro", 900_000),
-    ("gemini-2.5-pro-preview-05-06", "g2.5pro_vertex", 60_000),
+    ("openrouter/google/gemini-2.5-pro-preview", "g2.5pro", 60_000),
 )
