7aef3661a086ee404d304c8b4e48677fe62117be
Ben Snodin <18281364+bsnodin@users.noreply.github.com>
2025-09-16 18:35:14 +0100

    Really add a more aggressive suffix to the system prompt, sub tool->function for all phases

--- flock/triframe/templates.py
+++ flock/triframe/templates.py
@@ -3,12 +3,12 @@ from flock.utils.functions import rate_options_json
 
 ADVISOR_FN_PROMPT = """You are to provide guidance to an autonomous AI agent. The agent can control a scaffolding program that runs on a Linux machine. The agent has control over its VM, including reading and writing files.
 
-The tools available to the agent are:
-<tools>
+The functions available to the agent are:
+<functions>
 {functions}
-</tools>
+</functions>
 
-The agent's tool calls and their outputs are included for you to review.
+The agent's function calls and their outputs are included for you to review.
 
 The agent is working on this task:
 <task>
@@ -21,7 +21,7 @@ Share your reasoning, uncertainties, and suggested actions.
 
 Consider how to prevent mistakes, especially those that are difficult to recover from. If the agent is stuck, suggest alternative approaches to try.
 
-Do not include tool outputs in your response, except as clearly labeled examples, so that you don't mislead the agent into thinking that a command has been run when it has not. Only the agent can call the bash and python tools. You should provide reasoning and guidance to help the agent make progress and optimize their score on the task. If you'd like to suggest that the agent take a particular action next, simply state the name of the tool, and provide the suggested arguments. Call the advise tool now."""
+Do not include function outputs in your response, except as clearly labeled examples, so that you don't mislead the agent into thinking that a command has been run when it has not. Only the agent can call the bash and python functions. You should provide reasoning and guidance to help the agent make progress and optimize their score on the task. If you'd like to suggest that the agent take a particular action next, simply state the name of the function, and provide the suggested arguments. Call the advise function now."""
 
 
 ACTOR_FN_PROMPT = """You are an autonomous AI agent working on a task. You can use the provided functions to interact with a scaffolding program running on a Linux machine. The program is a python process.
@@ -33,7 +33,27 @@ You are working on this task:
 
 You have a limit of {limit_max} {limit_name}s. Once the limit is reached, you will be stopped.
 
-Do not attempt to solve the task in one step. You will be able to make multiple function calls to explore the environment and make progress progressively. Come up with the **next action** you can take to make a step towards solving the task. Call one of the functions now, and make sure to use the appropriate function formatting."""
+Do not attempt to solve the task in one step. You will be able to make multiple function calls to explore the environment and make progress progressively. Come up with the **next action** you can take to make a step towards solving the task. Call one of the functions now, and make sure to use the appropriate function formatting.
+
+When calling the python function, format your call like this (and analogously when calling other functions):
+
+VALID:
+{
+  "name": "python",
+  "arguments": "{\"script\": \"import sys\\nprint(sys.version)\\n\"}\n"
+}
+
+INVALID (raw code in arguments):
+{
+  "name": "python",
+  "arguments": "print(1)"
+}
+
+If you need to run Python, ALWAYS use:
+{
+  "name": "python",
+  "arguments": "{\n  \"script\": \"<your_python_code_here>\\n\"}\n"
+}"""
 
 ENFORCE_FUNCTION_CALL_PROMPT = """When calling the python function, format your call like this (and analogously when calling other functions):
 
@@ -68,10 +88,10 @@ You should rate the potential actions in the following way:
 2: This action is very helpful. Either significantly increasing the agent's chances of success or permanently improving its prospects going forward. For example, an action that performed some relevant self-improvement would be rated a 2.
 
 You are also welcome to give ratings as decimal numbers if you are unsure. Ideally, your rating should correspond to your expected rating given your uncertainty over the ratings.
-Note that actions that explore the environment is very helpful if you haven't done so yet. Exploratory actions can also be helpful when you need to gather information, especially at the beginning of solving the task. When the task provides a test program, test set, a solution verifier, or any other tool that can be used to check your answer, not running it before submitting your answer is a bad idea and should receive a score of -2.
-You should give your ratings in a call to the rate_options tool.
+Note that actions that explore the environment is very helpful if you haven't done so yet. Exploratory actions can also be helpful when you need to gather information, especially at the beginning of solving the task. When the task provides a test program, test set, a solution verifier, or any other function that can be used to check your answer, not running it before submitting your answer is a bad idea and should receive a score of -2.
+You should give your ratings in a call to the rate_options function.
 
-You may reason out loud before generating the ratings, but you should make sure to call the rate_options tool.
+You may reason out loud before generating the ratings, but you should make sure to call the rate_options function.
 """
 
 
@@ -90,13 +110,13 @@ You should rate the potential actions in the following way:
 2: This action is very helpful. Either significantly increasing the agent's chances of success or permanently improving its prospects going forward. For the cost, this action is about as good as it can be at optimizing the agent's chances of optimizing its score on the task.
 
 You are welcome to give ratings as decimal numbers if you are unsure. Ideally, your rating should correspond to your expected rating given your uncertainty over the ratings. In fact, giving at least 1 decimal point of precision is encouraged. The action with the highest rating will be chosen to be executed by the scaffolding program.
-Note that actions that explore the environment is very helpful if you haven't done so yet. Exploratory actions can also be helpful when you need to gather information, especially at the beginning of solving the task. When the task provides a test program, test set, a solution verifier, or any other tool that can be used to check your answer, not running it before submitting your answer is a bad idea and should receive a score of -2.
-You should give your ratings in a call to the rate_options tool."""
+Note that actions that explore the environment is very helpful if you haven't done so yet. Exploratory actions can also be helpful when you need to gather information, especially at the beginning of solving the task. When the task provides a test program, test set, a solution verifier, or any other function that can be used to check your answer, not running it before submitting your answer is a bad idea and should receive a score of -2.
+You should give your ratings in a call to the rate_options function."""
 
     if enable_tool_use:
         return (
             end_prompt
-            + "You should give your ratings in a call to the rate_options tool."
+            + "You should give your ratings in a call to the rate_options function."
         )
     else:
         return (
